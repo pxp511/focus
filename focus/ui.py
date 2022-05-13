d698a457cb4fc9c7fb7c23eeab8dbe06d7cca1a4
pxp <1800012955@pku.edu.cn>
2022-05-13 16:36:43 +0800

    message length; beautification beautification

--- focus/ui.py
+++ focus/ui.py
@@ -6,11 +6,31 @@ from focus.Robot import Robot, fetch_from_origin
 
 def hint(s: str):
     hint = Tk()
-    hint.geometry('360x100')
+    hint.geometry('360x120')
     hint.title('focus')
-    hint_label = Label(hint, text=s)
-    hint_label.pack()
+    hint.configure(bg='white')
+    hint_label = Label(hint, text=s, bg='white')
+    hint_label.pack(pady=40)
 
+
+def warnning(s: str):
+    warnning = Tk()
+    warnning.geometry('360x120')
+    warnning.title('focus')
+    warnning.configure(bg='white')
+    warnning_label = Label(warnning, text='\nWANNING', bg='white', fg='red')
+    warnning_label.pack()
+    warnning_content = Label(warnning, text=s, bg='white')
+    warnning_content.pack()
+
+
+def get_pos(chars: str, wraplength: int):
+    char_number = wraplength / 7
+    if len(chars) > char_number:
+        pos = 'w'
+    else:
+        pos = 'center'
+    return pos
 
 def main(robot: Robot):
     repository = robot._repository
@@ -18,12 +38,11 @@ def main(robot: Robot):
     global focus_file
     focus_file = f"{focus_dir}/focus.json"
     change_file = f"{focus_dir}/change.json"
-    history_file = f"{focus_dir}/history.json"
     history_count = 5
-    length = '600'
-    height = '400'
-
-
+    length = '1300'
+    height = '670'
+    
+    
     def add_focus_file():
         file_path = file_message_entry.get()
         if file_path == "":
@@ -38,7 +57,7 @@ def main(robot: Robot):
         if os.path.isfile(os.path.abspath(file_path)):
             hint("successfully add a focus file")
         else:
-            hint("WARNNING \n the file doesn't exist, but added anyway.\n i can't do correction for you, \n so make sure your path is right")
+            warnning("the file doesn't exist, but added anyway.\n i can't do correction for you, \n so make sure your path is right")
         focus_json["focus_file_list"].append(file_path)
         with open(focus_file, 'w') as f:
             json.dump(focus_json, f, indent=4)
@@ -59,8 +78,9 @@ def main(robot: Robot):
         if os.path.isdir(os.path.abspath(directory_path)):
             hint("successfully add a focus directory")
         else:
-            hint("WARNNING \n the directory doesn't exist, but added anyway.\n i can't do correction for you, \n so make sure your path is right")
+            warnning("the directory doesn't exist, but added anyway.\n i can't do correction for you, \n so make sure your path is right")
         focus_json["focus_directory_list"].append(directory_path)
+        print(focus_json["focus_directory_list"])
         with open(focus_file, 'w') as f:
             json.dump(focus_json, f, indent=4)
         renew()
@@ -73,6 +93,7 @@ def main(robot: Robot):
             return
         with open(focus_file, 'r') as f:
             focus_json = json.load(f)
+        delete_ok = False
         for file in focus_json["focus_file_list"]:
             if file_path == file:
                 focus_json["focus_file_list"].remove(file_path)
@@ -93,6 +114,7 @@ def main(robot: Robot):
             return
         with open(focus_file, 'r') as f:
             focus_json = json.load(f)
+        delete_ok = False
         for directory in focus_json["focus_directory_list"]:
             if directory_path == directory:
                 focus_json["focus_directory_list"].remove(directory_path)
@@ -119,27 +141,23 @@ def main(robot: Robot):
         else:
             count_of_history_for_show = len(change_list)
             ellipsis = ''
-        for widget in renew_history_display_panel.winfo_children():
+        for widget in history_display_panel.winfo_children():
             widget.destroy()
-        blank_label = Label(renew_history_display_panel, text="\n")
-        blank_label.grid(row=0, column=0, columnspan=6)
-        recent_change_label = Label(renew_history_display_panel, text="your focus change", fg='black', font=('Arial', 18))
-        recent_change_label.grid(row=1, columnspan=6)
-        type_label_topline = Label(renew_history_display_panel, text="type")
-        type_label_topline.grid(row=2, column=0)
-        path_label_topline = Label(renew_history_display_panel, text="path")
-        path_label_topline.grid(row=2, column=1)
-        stat_label_topline = Label(renew_history_display_panel, text="status")
-        stat_label_topline.grid(row=2, column=2)
-        time_label_topline = Label(renew_history_display_panel, text="time")
-        time_label_topline.grid(row=2, column=3)
-        author_label_topline = Label(renew_history_display_panel, text="author")
-        author_label_topline.grid(row=2, column=4)
-        message_label_topline = Label(renew_history_display_panel, text="message")
-        message_label_topline.grid(row=2, column=5)
-        file_label_topline = Label(renew_history_display_panel, text="file")
-        file_label_topline.grid(row=2, column=6)
-        row_number = 2
+        row_number = 0
+        type_label_topline = Label(history_display_panel, text="type", font=('Arial', 16), bg='white')
+        type_label_topline.grid(row=row_number, column=0, pady=15)
+        path_label_topline = Label(history_display_panel, text="path", font=('Arial', 16), bg='white')
+        path_label_topline.grid(row=row_number, column=1)
+        stat_label_topline = Label(history_display_panel, text="status", font=('Arial', 16), bg='white')
+        stat_label_topline.grid(row=row_number, column=2)
+        time_label_topline = Label(history_display_panel, text="time", font=('Arial', 16), bg='white')
+        time_label_topline.grid(row=row_number, column=3)
+        author_label_topline = Label(history_display_panel, text="author", font=('Arial', 16), bg='white')
+        author_label_topline.grid(row=row_number, column=4)
+        message_label_topline = Label(history_display_panel, text="message", font=('Arial', 16), bg='white')
+        message_label_topline.grid(row=row_number, column=5)
+        file_label_topline = Label(history_display_panel, text="file", font=('Arial', 16), bg='white')
+        file_label_topline.grid(row=row_number, column=6)
         for index in range(len(change_list) - 1, len(change_list) - count_of_history_for_show - 1, -1):
             row_number += 1
             record: dict = change_list[index]
@@ -153,32 +171,31 @@ def main(robot: Robot):
             author = record["change"]["author"]
             message = record["change"]["message"]
             file = record.get("file", "")
-            type_label = Label(renew_history_display_panel, text=type_of_record)
+            type_label = Label(history_display_panel, text=type_of_record, bg='white')
             type_label.grid(row=row_number, column=0)
-            path_label = Label(renew_history_display_panel, text=path)
+            wraplength = 300
+            pos = get_pos(path, wraplength)
+            path_label = Label(history_display_panel, text=path, width=30, height=3, wraplength=wraplength, anchor=pos, bg='white')
             path_label.grid(row=row_number, column=1)
-            stat_label = Label(renew_history_display_panel, text=stat)
+            stat_label = Label(history_display_panel, text=stat, bg='white')
             stat_label.grid(row=row_number, column=2)
-            time_label = Label(renew_history_display_panel, text=time)
+            time_label = Label(history_display_panel, text=time, width=20, bg='white')
             time_label.grid(row=row_number, column=3)
-            author_label = Label(renew_history_display_panel, text=author)
+            author_label = Label(history_display_panel, text=author, bg='white')
             author_label.grid(row=row_number, column=4)
-            message_label = Label(renew_history_display_panel, text=message)
+            wraplength = 300
+            pos = get_pos(message, wraplength)
+            message_label = Label(history_display_panel, text=message, width=30, height=3, wraplength=wraplength, anchor=pos, bg='white')
             message_label.grid(row=row_number, column=5)
-            file_label = Label(renew_history_display_panel, text=file)
+            wraplength = 300
+            pos = get_pos(file, wraplength)
+            file_label = Label(history_display_panel, text=file, width=30, height=3, wraplength=wraplength, anchor=pos, bg='white')
             file_label.grid(row=row_number, column=6)
         row_number += 1
         if ellipsis != '':
-            ellipsis_label = Label(renew_history_display_panel, text=ellipsis)
-            ellipsis_label.grid(row=row_number, column=0, columnspan=6)
-            row_number += 1
-        renew_button = Button(renew_history_display_panel, text='renew', command=renew)
-        renew_button.grid(row=row_number, column=1)
-        history_button = Button(renew_history_display_panel, text='show all', command=show_all_history)
-        history_button.grid(row=row_number, column=3)
-        fetch_button = Button(renew_history_display_panel, text='fetch', command=fetch)
-        fetch_button.grid(row=row_number, column=5)
-        renew_history_display_panel.pack()
+            ellipsis_label = Label(history_display_panel, text=ellipsis, font=('Arial', 25), bg='white')
+            ellipsis_label.grid(row=row_number, column=4)
+        history_display_panel.pack()
 
 
     def show_all_history():
@@ -190,22 +207,26 @@ def main(robot: Robot):
             return
         content_window = Tk()
         content_window.geometry(f'{length}x{height}')
-        content_window.title('all history')
-        type_label_topline = Label(content_window, text="type")
-        type_label_topline.grid(row=0, column=0)
-        path_label_topline = Label(content_window, text="path")
-        path_label_topline.grid(row=0, column=1)
-        stat_label_topline = Label(content_window, text="status")
-        stat_label_topline.grid(row=0, column=2)
-        time_label_topline = Label(content_window, text="time")
-        time_label_topline.grid(row=0, column=3)
-        author_label_topline = Label(content_window, text="author")
-        author_label_topline.grid(row=0, column=4)
-        message_label_topline = Label(content_window, text="message")
-        message_label_topline.grid(row=0, column=5)
-        file_label_topline = Label(content_window, text="file")
-        file_label_topline.grid(row=0, column=6)
+        content_window.configure(bg="white")
+        content_window.title('focus')
+        all_changes_label = Label(content_window, text="\nall changes", fg='black', font=('Arial', 18), bg='white')
+        all_changes_label.pack(pady=10)
+        content_frame = Frame(content_window, bg='white')
         row_number = 0
+        type_label_topline = Label(content_frame, text="type", font=('Arial', 16), bg='white')
+        type_label_topline.grid(row=row_number, column=0, pady=15)
+        path_label_topline = Label(content_frame, text="path", font=('Arial', 16), bg='white')
+        path_label_topline.grid(row=row_number, column=1)
+        stat_label_topline = Label(content_frame, text="status", font=('Arial', 16), bg='white')
+        stat_label_topline.grid(row=row_number, column=2)
+        time_label_topline = Label(content_frame, text="time", font=('Arial', 16), bg='white')
+        time_label_topline.grid(row=row_number, column=3)
+        author_label_topline = Label(content_frame, text="author", font=('Arial', 16), bg='white')
+        author_label_topline.grid(row=row_number, column=4)
+        message_label_topline = Label(content_frame, text="message", font=('Arial', 16), bg='white')
+        message_label_topline.grid(row=row_number, column=5)
+        file_label_topline = Label(content_frame, text="file", font=('Arial', 16), bg='white')
+        file_label_topline.grid(row=row_number, column=6)
         for index in range(len(change_list) - 1, -1, -1):
             row_number += 1
             record: dict = change_list[index]
@@ -219,20 +240,27 @@ def main(robot: Robot):
             author = record["change"]["author"]
             message = record["change"]["message"]
             file = record.get("file", "")
-            type_label = Label(content_window, text=type_of_record)
+            type_label = Label(content_frame, text=type_of_record, bg='white')
             type_label.grid(row=row_number, column=0)
-            path_label = Label(content_window, text=path)
+            wraplength = 300
+            pos = get_pos(path, wraplength)
+            path_label = Label(content_frame, text=path, width=30, height=3, wraplength=wraplength, anchor=pos, bg='white')
             path_label.grid(row=row_number, column=1)
-            stat_label = Label(content_window, text=stat)
+            stat_label = Label(content_frame, text=stat, bg='white')
             stat_label.grid(row=row_number, column=2)
-            time_label = Label(content_window, text=time)
+            time_label = Label(content_frame, text=time, width=20, bg='white')
             time_label.grid(row=row_number, column=3)
-            author_label = Label(content_window, text=author)
+            author_label = Label(content_frame, text=author, bg='white')
             author_label.grid(row=row_number, column=4)
-            message_label = Label(content_window, text=message)
+            wraplength = 300
+            pos = get_pos(message, wraplength)
+            message_label = Label(content_frame, text=message, width=30, height=3, wraplength=wraplength, anchor=pos, bg='white')
             message_label.grid(row=row_number, column=5)
-            file_label = Label(content_window, text=file)
+            wraplength = 300
+            pos = get_pos(file, wraplength)
+            file_label = Label(content_frame, text=file, width=30, height=3, wraplength=wraplength, anchor=pos, bg='white')
             file_label.grid(row=row_number, column=6)
+        content_frame.pack()
         content_window.mainloop()
 
 
@@ -243,29 +271,41 @@ def main(robot: Robot):
     root = Tk()
     root.geometry(f'{length}x{height}')
     root.title('focus')
-    add_delete_grid = Frame(root)
-    span = 2
-    file_message_label = Label(add_delete_grid, text="file path:")
-    file_message_label.grid(row=0, column=0)
-    file_message_entry = Entry(add_delete_grid)
-    file_message_entry.grid(row=0, column=1, columnspan=span)
+    root.configure(bg='white')
+    
+    blank_line = Label(root, text=" ", bg='white')
+    blank_line.pack(pady=10)
+    add_delete_grid = Frame(root, bg='white')
+    file_message_label = Label(add_delete_grid, text="file path:", bg='white')
+    file_message_label.grid(row=0, column=0, pady=0)
+    file_message_entry = Entry(add_delete_grid, bg='white')
+    file_message_entry.grid(row=0, column=1)
     file_message_add = Button(add_delete_grid, text='add', command=add_focus_file)
-    file_message_add.grid(row=0, column=(span + 1))
-    file_message_delete = Button(add_delete_grid, text='delete', command=delete_focus_file)
-    file_message_delete.grid(row=0, column=(span + 2))
-    dir_message_label = Label(add_delete_grid, text="directory path:")
-    dir_message_label.grid(row=1, column=0)
-    dir_message_entry = Entry(add_delete_grid)
-    dir_message_entry.grid(row=1, column=1, columnspan=span)
-    dir_message_add = Button(add_delete_grid, text='add', command=add_focus_directory)
-    dir_message_add.grid(row=1, column=(span + 1))
-    dir_message_delete = Button(add_delete_grid, text='delete', command=delete_focus_directory)
-    dir_message_delete.grid(row=1, column=(span + 2))
+    file_message_add.configure(bg="white")
+    file_message_add.grid(row=0, column=2)
+    file_message_delete = Button(add_delete_grid, text='delete', command=delete_focus_file, bg='black')
+    file_message_delete.grid(row=0, column=3)
+    dir_message_label = Label(add_delete_grid, text="directory path:", bg='white')
+    dir_message_label.grid(row=1, column=0, pady=12)
+    dir_message_entry = Entry(add_delete_grid, bg='white')
+    dir_message_entry.grid(row=1, column=1)
+    dir_message_add = Button(add_delete_grid, text='add', command=add_focus_directory, bg='white')
+    dir_message_add.grid(row=1, column=2)
+    dir_message_delete = Button(add_delete_grid, text='delete', command=delete_focus_directory, bg='white')
+    dir_message_delete.grid(row=1, column=3)
     add_delete_grid.pack()
-    renew_history_display_panel = Frame(root)
+    recent_change_label = Label(root, text="\nyour focus change", fg='black', font=('Arial', 18), bg='white')
+    recent_change_label.pack(pady=10)
+    history_display_panel = Frame(root, bg='white')
     renew()
+    blank_line = Label(root, text="", bg='white')
+    blank_line.pack(pady=10)
+    renew_history_fetch_button_panel = Frame(root)
+    renew_button = Button(renew_history_fetch_button_panel, text='renew', width=10, height=2, command=renew, bg='white')
+    renew_button.pack(side="left")
+    history_button = Button(renew_history_fetch_button_panel, text='show all', width=10, height=2, command=show_all_history, bg='white')
+    history_button.pack(side="left")
+    fetch_button = Button(renew_history_fetch_button_panel, text='fetch', width=10, height=2, command=fetch, bg='white')
+    fetch_button.pack(side="left")
+    renew_history_fetch_button_panel.pack()
     mainloop()
-
-
-if __name__ == '__main__':
-    main()
